e6b042ef0926e944d803af7ad613a5fcac7f26c4
Fritz Reichwald <reichwald@b1-systems.de>
2026-04-13 12:53:46 +0200

    New upstream version 3.7.0

--- qutebrowser/__init__.py
+++ qutebrowser/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+# SPDX-FileCopyrightText: Freya Bruhin (The Compiler) <mail@qutebrowser.org>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -9,12 +9,12 @@ import datetime
 
 _year = datetime.date.today().year
 
-__author__ = "Florian Bruhin"
-__copyright__ = "Copyright 2013-{} Florian Bruhin (The Compiler)".format(_year)
+__author__ = "Freya Bruhin"
+__copyright__ = "Copyright 2013-{} Freya Bruhin (The Compiler)".format(_year)
 __license__ = "GPL-3.0-or-later"
 __maintainer__ = __author__
 __email__ = "mail@qutebrowser.org"
-__version__ = "3.6.3"
+__version__ = "3.7.0"
 __version_info__ = tuple(int(part) for part in __version__.split('.'))
 __description__ = "A keyboard-driven, vim-like browser based on Python and Qt."
 
